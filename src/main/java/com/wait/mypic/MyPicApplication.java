bb91de3044f8a696defc8a824eec87a3e66752d0
collinewait <collinewait17@gmail.com>
2019-09-08 15:50:30 +0300

    initial commit

---
+++ src/main/java/com/wait/mypic/MyPicApplication.java
@@ -0,0 +1,13 @@
+package com.wait.mypic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPicApplication.class, args);
+	}
+
+}
