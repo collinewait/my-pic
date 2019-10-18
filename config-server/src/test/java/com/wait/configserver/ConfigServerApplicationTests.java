5a8b98579ca892dbd77438442c400de1bbb695bd
collinewait <collinewait17@gmail.com>
2019-10-18 14:47:49 +0300

    build a config server

---
+++ config-server/src/test/java/com/wait/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wait.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
