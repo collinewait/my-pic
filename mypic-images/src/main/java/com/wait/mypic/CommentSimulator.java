c7a8a2f7e8011f30c288c3ebb124d92868ad8789
collinewait <collinewait17@gmail.com>
2019-10-18 14:06:53 +0300

    update commentSimulator with better names
    also add a simulator to reload the page every 500ms

--- mypic-images/src/main/java/com/wait/mypic/CommentSimulator.java
+++ mypic-images/src/main/java/com/wait/mypic/CommentSimulator.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.support.BindingAwareModelMap;
 
 import com.wait.mypic.images.Comment;
 import com.wait.mypic.images.CommentController;
@@ -21,30 +22,43 @@ import reactor.core.publisher.Mono;
 @Profile("simulator")
 @Component
 public class CommentSimulator {
-	private final CommentController controller;
+
+	private final HomeController homeController;
+	private final CommentController commentController;
 	private final ImageRepository repository;
 
 	private final AtomicInteger counter;
 
-	public CommentSimulator(CommentController controller,
-			ImageRepository repository) {
-		this.controller = controller;
+	public CommentSimulator(HomeController homeController,
+			CommentController commentController, ImageRepository repository) {
+		this.homeController = homeController;
+		this.commentController = commentController;
 		this.repository = repository;
 		this.counter = new AtomicInteger(1);
 	}
 
-	@EventListener // signals Spring to pipe application events issued to the app
-									// context.
-	public void onApplicationReadyEvent(ApplicationReadyEvent event) {
+	@EventListener
+	public void simulateComments(ApplicationReadyEvent event) {
 		Flux.interval(Duration.ofMillis(1000)).flatMap(tick -> repository.findAll())
 				.map(image -> {
 					Comment comment = new Comment();
 					comment.setImageId(image.getId());
 					comment.setComment("Comment #" + counter.getAndIncrement());
 					return Mono.just(comment);
-				})
-				.flatMap(
-						newComment -> Mono.defer(() -> controller.addComment(newComment)))
+				}).flatMap(newComment -> Mono
+						.defer(() -> commentController.addComment(newComment)))
+				.subscribe();
+	}
+
+	/*
+	 * simulates user loading the home page every 500ms
+	 * Apparently not working as expected. I have used a browser extension to simulate this
+	 */
+	@EventListener
+	public void simulateUsersClicking(ApplicationReadyEvent event) {
+		Flux.interval(Duration.ofMillis(500))
+				.flatMap(tick -> Mono
+						.defer(() -> homeController.index(new BindingAwareModelMap())))
 				.subscribe();
 	}
 }
