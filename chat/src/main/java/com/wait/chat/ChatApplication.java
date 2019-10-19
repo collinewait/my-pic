6e0f33f60585c648855f53cb0f19d2343cf6573b
collinewait <collinewait17@gmail.com>
2019-10-19 14:25:59 +0300

    bootstrap a chat service to handle WebSocket traffic

---
+++ chat/src/main/java/com/wait/chat/ChatApplication.java
@@ -0,0 +1,15 @@
+package com.wait.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringCloudApplication
+@EnableEurekaClient
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
