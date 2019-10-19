892b57fb75cda1c5920fd3713187f76498174015
collinewait <collinewait17@gmail.com>
2019-10-19 14:22:33 +0300

    forward the incoming stream of comments

--- mypic-comments/src/main/java/com/wait/mypic/comments/CommentService.java
+++ mypic-comments/src/main/java/com/wait/mypic/comments/CommentService.java
@@ -1,5 +1,7 @@
 package com.wait.mypic.comments;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
@@ -11,7 +13,6 @@ import org.springframework.stereotype.Service;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 /**
  * @author collinewaitire 25 Sep 2019
@@ -20,24 +21,23 @@ import reactor.core.publisher.Mono;
 @EnableBinding(Processor.class)
 public class CommentService {
 
+	private static final Logger log = LoggerFactory.getLogger(CommentService.class);
+
 	private CommentRepository repository;
 	private final MeterRegistry meterRegistry;
 
-	public CommentService(CommentRepository repository,
-			MeterRegistry meterRegistry) {
+	public CommentService(CommentRepository repository, MeterRegistry meterRegistry) {
 		this.repository = repository;
 		this.meterRegistry = meterRegistry;
 	}
 
 	@StreamListener
 	@Output(Processor.OUTPUT)
-	public Flux<Void> save(
-			@Input(Processor.INPUT) Flux<Comment> newComments) {
-		return repository.saveAll(newComments).flatMap(comment -> {
-			meterRegistry
-					.counter("comments.consumed", "imageId", comment.getImageId())
-					.increment();
-			return Mono.empty();
+	public Flux<Comment> save(@Input(Processor.INPUT) Flux<Comment> newComments) {
+		return repository.saveAll(newComments).map(comment -> {
+			log.info("Saving new comment " + comment);
+			meterRegistry.counter("comments.consumed", "imageId", comment.getImageId()).increment();
+			return comment;
 		});
 	}
 
