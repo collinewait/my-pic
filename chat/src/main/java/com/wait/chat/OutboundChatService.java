178072374db2f7cf8b318df2fe6872442e38dd14
collinewait <collinewait17@gmail.com>
2019-10-20 17:58:07 +0300

    apply a filter to OutboundChatService

--- chat/src/main/java/com/wait/chat/OutboundChatService.java
+++ chat/src/main/java/com/wait/chat/OutboundChatService.java
@@ -4,8 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketSession;
 
 import reactor.core.publisher.Flux;
@@ -17,20 +17,21 @@ import reactor.core.publisher.Mono;
  */
 @Service
 @EnableBinding(ChatServiceStreams.class)
-public class OutboundChatService implements WebSocketHandler {
+public class OutboundChatService extends UserParsingHandshakeHandler {
 
 	private final static Logger log = LoggerFactory.getLogger(CommentService.class);
 
-	private Flux<String> flux;
-	private FluxSink<String> chatMessageSink;
+	private Flux<Message<String>> flux;
+	private FluxSink<Message<String>> chatMessageSink;
 
 	public OutboundChatService() {
-		this.flux = Flux.<String>create(emitter -> this.chatMessageSink = emitter, FluxSink.OverflowStrategy.IGNORE)
+		this.flux = Flux
+				.<Message<String>>create(emitter -> this.chatMessageSink = emitter, FluxSink.OverflowStrategy.IGNORE)
 				.publish().autoConnect();
 	}
 
 	@StreamListener(ChatServiceStreams.BROKER_TO_CLIENT)
-	public void listen(String message) {
+	public void listen(Message<String> message) {
 		if (chatMessageSink != null) {
 			log.info("Publishing " + message + " to wesocket");
 			chatMessageSink.next(message);
@@ -38,8 +39,30 @@ public class OutboundChatService implements WebSocketHandler {
 	}
 
 	@Override
-	public Mono<Void> handle(WebSocketSession session) {
-		return session.send(this.flux.map(session::textMessage).log("outbound-wrapp-as-websocket-message"))
-				.log("outbound-publish-to-websocket");
+	protected Mono<Void> handleInternal(WebSocketSession session) {
+		return session.send(this.flux.filter(s -> validate(s, getUser(session.getId()))).map(this::transform)
+				.map(session::textMessage).log(getUser(session.getId()) + "-outbound-wrap-as-websocket-message"))
+				.log(getUser(session.getId()) + "-outbound-publish-to-websocket");
+	}
+
+	private boolean validate(Message<String> message, String user) {
+		if (message.getPayload().startsWith("@")) {
+			String targetUser = message.getPayload().substring(1, message.getPayload().indexOf(" "));
+
+			String sender = message.getHeaders().get(ChatServiceStreams.USER_HEADER, String.class);
+
+			return user.equals(sender) || user.equals(targetUser);
+		} else {
+			return true;
+		}
+	}
+
+	private String transform(Message<String> message) {
+		String user = message.getHeaders().get(ChatServiceStreams.USER_HEADER, String.class);
+		if (message.getPayload().startsWith("@")) {
+			return "(" + user + "): " + message.getPayload();
+		} else {
+			return "(" + user + ")(all): " + message.getPayload();
+		}
 	}
 }
